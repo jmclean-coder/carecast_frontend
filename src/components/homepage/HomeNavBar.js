2e54657f927e40c542e040b13dfc56559e556db2
Joshua Mclean <joshmcleana@gmail.com>
2020-07-20 15:09:38 -0700

    use Link containers with NavBar

--- src/components/homepage/HomeNavBar.js
+++ src/components/homepage/HomeNavBar.js
@@ -1,20 +1,23 @@
-import React from 'react';
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
-import { LinkContainer } from 'react-router-bootstrap'
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { LinkContainer } from "react-router-bootstrap";
+
 
 export default function HomeNavBar() {
-    return(
-<Navbar bg="light" expand="lg">
-  <Navbar.Brand href="/">CareCast</Navbar.Brand>
-  <Navbar.Toggle aria-controls="basic-navbar-nav" />
-  <Navbar.Collapse id="basic-navbar-nav">
-    <Nav className="mr-auto">
-      <LinkContainer to="login">
-      <Nav.Link >Login</Nav.Link>
+  return (
+    <Navbar bg="light" expand="lg">
+      <LinkContainer to="/">
+        <Navbar.Brand>CareCast</Navbar.Brand>
       </LinkContainer>
-    </Nav>
-  </Navbar.Collapse>
-</Navbar>
-    )
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <LinkContainer to="login" >
+            <Nav.Link>Login</Nav.Link>
+          </LinkContainer>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
 }
