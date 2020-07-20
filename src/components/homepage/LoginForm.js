8ba197a83c4c66990168a20857295835ae677bba
Joshua Mclean <joshmcleana@gmail.com>
2020-07-20 16:05:17 -0700

    Handle CHange and Handle Submit for Login

--- src/components/homepage/LoginForm.js
+++ src/components/homepage/LoginForm.js
@@ -1,26 +1,44 @@
 import React from "react";
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import {api} from '../../services/api';
 
 export default class LoginForm extends React.Component {
   state = {
-    username: "",
-    password: "",
+    error: false,
+    fields: {
+      username: "",
+      password: "",
+    },
   };
 
+  handleChange = e =>{
+    const newFields = {...this.state.fields, [e.target.name]: e.target.value}
+    this.setState({
+      fields: newFields
+    })
+  }
+
+  handleSubmit = e =>{
+    e.preventDefault();
+    //calling the login method from api.auth object in services/api
+    api.auth.login(this.state.fields)
+    .then(data => this.props.onLogin(data))
+  }
+
+
   render() {
+    // console.log(api)
+    const {fields} = this.state
     return (
-      <Form>
+      <Form onSubmit={this.handleSubmit}>
         <Form.Group controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We'll never share your email with anyone else.
-          </Form.Text>
+          <Form.Label>Username</Form.Label>
+          <Form.Control  onChange={this.handleChange} name="username" type="username" placeholder="Enter username" value={fields.username} />
         </Form.Group>
         <Form.Group controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control onChange={this.handleChange} name="password" type="password" placeholder="Password" value={fields.password}/>
         </Form.Group>
         <Button variant="primary" type="submit">
           Submit
