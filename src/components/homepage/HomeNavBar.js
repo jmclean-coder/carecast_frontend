3602856bb8a04f6cb0a417d2deb113edf779077c
Joshua Mclean <joshmcleana@gmail.com>
2020-07-21 01:15:43 -0700

    Update Navbar to conditionally render

--- src/components/homepage/HomeNavBar.js
+++ src/components/homepage/HomeNavBar.js
@@ -1,24 +1,52 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
-import { LinkContainer } from "react-router-bootstrap";
-
+import { Link } from "react-router-dom";
 
 export default function HomeNavBar(props) {
-const token = localStorage.getItem("token");
+  const token = localStorage.getItem("token");
 
-const handleLogout = () => props.onLogout()
-
+  const handleLogout = () => props.onLogout();
 
   return (
-    <Navbar bg="light" expand="lg">
-      <LinkContainer to="/">
-        <Navbar.Brand>CareCast</Navbar.Brand>
-      </LinkContainer>
+    <Navbar collapseOnSelect expand="lg" bg="light" >
+      <Navbar.Brand as={Link} to="/">
+        CareCast
+      </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          {token ? <Nav.Link onClick={handleLogout}>Sign Out</Nav.Link>: <LinkContainer to="login" ><Nav.Link>Login</Nav.Link></LinkContainer>}
+        <Nav>
+          {token ? (
+            <Nav.Link as={Link} to="/dashboard">
+              Dashboard
+            </Nav.Link>
+          ) : null}
+        
+          {token ? (
+            <Nav.Link as={Link} to="/journal">
+              Journal
+            </Nav.Link>
+          ) : null}
+        
+          {token ? (
+            <Nav.Link as={Link} to="/feeling_tracker">
+              Feelings Tracker
+            </Nav.Link>
+          ) : null}
+        
+          {token ? (
+            <Nav.Link as={Link} to="/todos">
+              To-Do List
+            </Nav.Link>
+          ) : null}
+        
+          {token ? (
+            <Nav.Link onClick={handleLogout}>Sign Out</Nav.Link>
+          ) : (
+            <Nav.Link as={Link} to="/login">
+              Login
+            </Nav.Link>
+          )}
         </Nav>
       </Navbar.Collapse>
     </Navbar>
