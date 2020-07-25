e0f1942784a7454b9add0fa173f3386645a40f99
Joshua Mclean <joshmcleana@gmail.com>
2020-07-25 16:51:26 -0700

    Build initial Feelings Page

--- src/containers/FeelingPage.js
+++ src/containers/FeelingPage.js
@@ -1,11 +1,22 @@
 import React from "react";
+import BlankCalendar from '../assets/svgComponents/BlankCalendar'
 import PrivacyHOC from '../HOCs/PrivacyHOC'
+import Calendar from '../components/feeling tracker/Calendar'
+import { Container } from "react-bootstrap";
 class FeelingPage extends React.Component {
   render() {
     return (
+      <Container>
+        
       <div>
-        <h1>Feeling Page Temp</h1>
+       <BlankCalendar ></BlankCalendar>
       </div>
+
+      <div>
+        <Calendar />
+      </div>
+
+      </Container>
     );
   }
 }
