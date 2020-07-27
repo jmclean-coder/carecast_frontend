490622b883147ac51e3c19e6a71a1a9c090b2cbe
Joshua Mclean <joshmcleana@gmail.com>
2020-07-27 02:58:59 -0700

    Move 'See More" button to Accordion header

--- src/components/dashboard/JournalAccordion.js
+++ src/components/dashboard/JournalAccordion.js
@@ -1,33 +1,32 @@
 import React, { useState } from "react";
-import {Link} from 'react-router-dom'
-import JournalCard from './JournalCard'
-import {
-    Container,
-    Accordion,
-    Card,
-    Button,
-  } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import JournalCard from "./JournalCard";
+import { Container, Accordion, Card, Button } from "react-bootstrap";
 
-  
-  
-  export default function JournalAccordion(props) {
-      const renderJournalEntries = () =>{
-          return props.journalEntries.map(entry=> <JournalCard updateJournalEntry={props.updateJournalEntry} addJournalEntry={props.addJournalEntry} journal={entry} key={`entry_${entry.id}`} />)
-      }
+export default function JournalAccordion(props) {
+  const renderJournalEntries = () => {
+    return props.journalEntries.map((entry) => (
+      <JournalCard
+        updateJournalEntry={props.updateJournalEntry}
+        addJournalEntry={props.addJournalEntry}
+        journal={entry}
+        key={`entry_${entry.id}`}
+      />
+    ));
+  };
   return (
     <Accordion defaultActiveKey="3">
       <Card>
         <Accordion.Toggle as={Card.Header} bg="dark" eventKey="3">
-          Journal
+          Journal{" "}
+          <Button variant="link" as={Link} to="/journal">
+            See More
+          </Button>
         </Accordion.Toggle>
         <Accordion.Collapse eventKey="3">
           <Container>
-            <Container className="text-right">
-            <Button  variant="link" as={Link} to="/journal">
-              See More
-            </Button>
-            </Container>
-         {renderJournalEntries()}
+            <Container className="text-right"></Container>
+            {renderJournalEntries()}
           </Container>
         </Accordion.Collapse>
       </Card>
