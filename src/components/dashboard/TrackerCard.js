1c7998f70028a8ec5edfed582d5a75331a196b16
Joshua Mclean <joshmcleana@gmail.com>
2020-07-27 00:00:33 -0700

    Refactor TrackerCard to be a controlled component

--- src/components/dashboard/TrackerCard.js
+++ src/components/dashboard/TrackerCard.js
@@ -11,19 +11,33 @@ import {
 
 import { ReactComponent as Plus } from "../../assets/BookPlus.svg";
 import { ReactComponent as Minus } from "../../assets/BookMinus.svg";
-export default function TrackerCard(props) {
-  return props.category.detailed ? (
-    <Card>
-      <Card.Body>
-        <Card.Title>{props.category.name}</Card.Title>
+export default class TrackerCard extends React.Component {
+  state = {
+    water: "",
+    sleep: "",
+    energy: "",
+    stress: "",
+    motivation: "",
+    productivity: ""
+  }
 
-        <Container onClick={() => props.incrementRating(props.ratingData)}>
+
+
+
+  render(){
+
+    return this.props.category.detailed ? (
+      <Card>
+      <Card.Body>
+        <Card.Title>{this.props.category.name}</Card.Title>
+
+        <Container onClick={() => this.props.incrementRating(this.props.ratingData)}>
           <Plus as={Button} name="plusBtn" />
         </Container>
 
-        <Card.Text>{props.ratingData.rating} units</Card.Text>
+        <Card.Text>0</Card.Text>
 
-        <Container onClick={() => props.decrementRating(props.ratingData)}>
+        <Container onClick={() => this.props.decrementRating(this.props.ratingData)}>
           <Minus as={Button} name="minusBtn" />
         </Container>
       </Card.Body>
@@ -31,18 +45,21 @@ export default function TrackerCard(props) {
   ) : (
     <Card>
       <Card.Body>
-        <Card.Title>{props.category.name}</Card.Title>
+        <Card.Title>{this.props.category.name}</Card.Title>
 
-        <Container onClick={() => props.incrementRating(props.ratingData)}>
+        <Container onClick={() => this.props.incrementRating(this.props.ratingData)}>
           <Plus as={Button} />
         </Container>
 
-        <Card.Text>{props.ratingData.rating}</Card.Text>
+        <Card.Text>0</Card.Text>
 
-        <Container onClick={() => props.decrementRating(props.ratingData)}>
+        <Container onClick={() => this.props.decrementRating(this.props.ratingData)}>
           <Minus as={Button} />
         </Container>
       </Card.Body>
     </Card>
   );
+}
+
+  
 }
