53f664dce4803f86fe467ae37b45948142719e3b
Joshua Mclean <joshmcleana@gmail.com>
2020-08-14 05:19:06 -0700

    create Footer component

---
+++ src/components/homepage/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {ReactComponent as LogoBranded} from '../../assets/svgComponents/Icons/LogoBranded.svg'
+export default function Footer() {
+    return (
+        <div className="page-footer">
+            
+            <LogoBranded className="footerlogo"/>
+        </div>
+    )
+}
