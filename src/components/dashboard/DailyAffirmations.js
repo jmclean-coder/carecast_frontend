d927b76ab05d7553b366ae73aa4fa4c7bd977bcd
Joshua Mclean <joshmcleana@gmail.com>
2020-08-14 05:11:37 -0700

    Import Dashboard styles, dynamic quotes

--- src/components/dashboard/DailyAffirmations.js
+++ src/components/dashboard/DailyAffirmations.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Jumbotron from 'react-bootstrap/Jumbotron'
+import './dashboard.css'
+
 export default function DailyAffirmations(props) {
   return (
     <Jumbotron fluid>
     <blockquote className="blockquote mb-0">
       <h2>
-        {' '}{ "And Still You Rise" || props.quoteOfDay.contents.quotes[0].quote}{' '}
+        {' '}{props.quoteOfDay.contents.quotes[0].quote}{' '}
       </h2>
       <footer className="blockquote-footer">
       {props.quoteOfDay.contents.quotes[0].author}<cite title="Source Title"> They Said So</cite>
