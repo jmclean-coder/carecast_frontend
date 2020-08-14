067c0cbce56dc6ceb10bf4f6df133de960c1f3ce
Joshua Mclean <joshmcleana@gmail.com>
2020-08-14 05:18:18 -0700

    Import styles, conditional rendering of tracker cards

--- src/components/dashboard/TrackerCard.js
+++ src/components/dashboard/TrackerCard.js
@@ -8,79 +8,74 @@ import {
   ListGroup,
   Button,
 } from "react-bootstrap";
+import "./dashboard.css";
 
 import { ReactComponent as Plus } from "../../assets/BookPlus.svg";
 import { ReactComponent as Minus } from "../../assets/BookMinus.svg";
 
 export default class TrackerCard extends React.Component {
+  static defaultProps = {
+    ratingData: {
+      rating: 0,
+    },
+  };
 
+  getRating = () => {
+    let categoryRating = this.props.ratingData.find(
+      (rating) => rating.category_id === this.props.category.id
+    );
+    return categoryRating;
+  };
 
- static defaultProps = {
-  ratingData: {
-    rating: 0,
-  }
-}
-
-getRating = () => {
-  let categoryRating = this.props.ratingData.find(rating => rating.category_id === this.props.category.id)
-  return categoryRating
-
-}
-
-  
- 
- render() {
-  //  console.log(this.getRating())
-    let ratingNum  
-    this.getRating() === undefined ? ratingNum = 0 : ratingNum = this.getRating().rating
+  render() {
+    //  console.log(this.getRating())
+    let ratingNum;
+    this.getRating() === undefined
+      ? (ratingNum = 0)
+      : (ratingNum = this.getRating().rating);
     // console.log(ratingNum)
 
-   return this.props.category.detailed ? (
-     <Card>
+    return this.props.category.detailed ? (
+      <Card className={`${this.props.category.name}`}>
         <Card.Body>
-          <Card.Title>{this.props.category.name}</Card.Title>
+          <Card.Title className="tracker-title">{this.props.category.name}</Card.Title>
 
           <Container
             onClick={() => this.props.incrementRating(this.props.category.id)}
-            >
-            <Plus as={Button} name="plusBtn" />
+          >
+            <Plus className="plus" as={Button} name="plusBtn" />
           </Container>
 
           {this.props.category.name === "Water" && (
             <Card.Text>{ratingNum} Glasses</Card.Text>
-            )}
+          )}
           {this.props.category.name === "Sleep" && (
-            <Card.Text>{ratingNum} Hours</Card.Text>
-            )}
+            <Card.Text className="tracker-text">{ratingNum} Hours</Card.Text>
+          )}
 
           <Container
             onClick={() => this.props.decrementRating(this.props.category.id)}
-            >
-            <Minus as={Button} name="minusBtn" />
+          >
+            <Minus className="minus" as={Button} name="minusBtn" />
           </Container>
         </Card.Body>
       </Card>
     ) : (
-      <Card>
+      <Card className={`${this.props.category.name}`} >
         <Card.Body>
-          <Card.Title>{this.props.category.name}</Card.Title>
+          <Card.Title className="tracker-title">{this.props.category.name}</Card.Title>
 
-          <Container
-            onClick={() => this.props.incrementRating(this.props.category.id)}
-            >
-            <Plus as={Button} />
+          <Container >
+            <Plus className="plus" onClick={() => this.props.incrementRating(this.props.category.id)} as={Button} /> <Minus  className="minus" onClick={() => this.props.decrementRating(this.props.category.id)} as={Button} />
           </Container>
 
-          <Card.Text>{ratingNum}/10</Card.Text>
+          <Card.Text className="tracker-text">{ratingNum}/10</Card.Text>
 
-          <Container
-            onClick={() => this.props.decrementRating(this.props.category.id)}
-            >
-            <Minus as={Button} />
-          </Container>
+          
+          
+        
         </Card.Body>
       </Card>
     );
-  };
+  }
 }
-    
