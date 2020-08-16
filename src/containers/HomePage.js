ca455a61f51f78f6cb6f53667a6e6affa5b172a8
Joshua Mclean <joshmcleana@gmail.com>
2020-08-16 03:14:24 -0700

    Style homepage content

--- src/containers/HomePage.js
+++ src/containers/HomePage.js
@@ -1,21 +1,22 @@
 import React from "react";
-import CardDeck from "react-bootstrap/CardDeck";
-import Card from "react-bootstrap/Card";
-import { Button, Container, Row, Col } from "react-bootstrap";
+
+import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import { ReactComponent as HeroImage } from "../assets/svgComponents/images/HeroImage(M).svg";
 import { ReactComponent as HomepageAboutImg } from "../assets/svgComponents/images/HomepageAboutImg.svg";
 import { ReactComponent as BoxSmileyBig} from "../assets/svgComponents/Icons/BoxsmileyBig.svg";
+import { ReactComponent as RedPen} from "../assets/svgComponents/Icons/RedPen.svg";
+import { ReactComponent as Book} from "../assets/svgComponents/Icons/Book.svg";
 // import './App.css';
-import { Icon, InlineIcon } from '@iconify/react';
+
 import bookIcon from '@iconify/icons-cil/book'
 import writeIcon from '@iconify/icons-jam/write';
 import Footer from '../components/homepage/Footer'
 
 function HomePage(props) {
   return (
-    <div className="header">
-      <div className="t">
+    <div className="hm-wrapper">
+
         <div>
           <h1 className="header-h1">Care Cast</h1>
           <HeroImage className="home-hero" />
@@ -23,45 +24,41 @@ function HomePage(props) {
             Give yourself the gift of care today.
           </h3>
         </div>
+
         <div className="home-btn-container">
         <Button className="btn-home-page" onClick={()=> props.history.push("/signup")}>Signup</Button>
         </div>
-      </div>
-        <h2 className="home-sub-head">
-          What We Offer
-        </h2>
 
+
+        <h2 className="home-sub-head">  What We Offer</h2>
       <div className="hm-content-wrapper">
-        <div className="text-center">
-        <Icon icon={bookIcon} style={{color: '#f4cd9c', fontSize: '86px'}} />
+        
+        <div className=" hm-content-card ">
+        <Book />
           <h2 className="text-center"> Journal</h2>
           <p className="home-content-p"> Release your inner thoughts. Write it out.</p>
+        <hr className="hm-line-divider"></hr>
         </div>
 
-        <div>
-          <div className="text-center">
-          <Icon icon={writeIcon} style={{color: '#ed8989', fontSize: '86px'}} />
-
-          </div>
-
-          <h2 className="text-center">Care Tracker</h2>
+        <div className=" hm-content-card " >
+          <RedPen />
+          <h2 className="text-center hm-content-card ">Care Tracker</h2>
           <p className="home-content-p">Stay on top of your self-care by tracking your habits.</p>
-        </div>
-
-        <div>
-          <div className="text-center">
-            <BoxSmileyBig/>
-          </div>
-
+       <hr className="hm-line-divider"></hr>
+       </div>
+       
+        <div className="hm-content-card">
+          <BoxSmileyBig/>
           <h2 className="text-center"> Daily Affirmations</h2>
           <p className="home-content-p">Set the Tone and start your day on a positive note</p>
         </div>
+
       </div>
 
-      <div className="about-us">
         <h2 className="about-us-subhead">About Us</h2>
-        <HomepageAboutImg className="home-about"/>
+        
         <div className='closing-content-wrapper'>
+        <HomepageAboutImg className="home-about"/>
           <p className="home-content-p">
           CareCast loves to do one thing, take care of you! You know who you are, you're the hardworking busybody, the person seeking inner connection, the envrionmentalist that's doing their part to save trees. You have a lot on your plate, and we're here to help you make sense of it in the good times and bad times. Because we <em>care.</em> 
           </p>
@@ -69,7 +66,7 @@ function HomePage(props) {
           <Button className='btn-home-page'>Learn More</Button>
           </div>
         </div>
-      </div>
+     
 
 
       <Footer/>
