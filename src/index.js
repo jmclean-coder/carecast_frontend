8a7deec1b9cd51915812f4024d65e80647a2c821
Joshua Mclean <joshmcleana@gmail.com>
2020-08-18 16:05:07 -0700

    Updates to use SASS

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import './index.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './index.css';
 
 
 
