09f2d985288eae1cc072421bc71f80fb9b110467
Joshua Mclean <joshmcleana@gmail.com>
2020-08-26 08:18:28 -0700

    Chnage from Container to divs

--- src/components/dashboard/DailyAffirmations.js
+++ src/components/dashboard/DailyAffirmations.js
@@ -1,29 +1,12 @@
 import React from "react";
-import Container from 'react-bootstrap/Container'
-import './dashboard.css'
 
+import "./dashboard.css";
 
 export default function DailyAffirmations(props) {
-
-  const containerStyle = {
-    width: "100%",
-    height: "5em",
-    paddingRight: "0.9375em",
-    paddingLeft: "0.9375em",
-    paddingBottom: "1em",
-    marginRight:" auto",
-    marginLeft: "auto",
-    textAlign: "center",
-  }
   return (
-      <Container style={containerStyle}>
-        <Container>
-  <h2>Hello, {props.firstName}</h2>
-        </Container>
-      <p>
-        "{props.affirmation}."
-      </p>
-      </Container>
-  
+    <div className="dsh-affirm-wrapper">
+        <h2>Hello, {props.firstName}</h2>
+      <p>"{props.affirmation}."</p>
+    </div>
   );
 }
