466f3745f25e7c276a1ce0e37c236ddceeb9f6e7
Joshua Mclean <joshmcleana@gmail.com>
2020-09-14 17:41:22 -0700

    Properly find and render Feeling cards, remove line breaks

--- src/components/feeling tracker/MoodCard.js
+++ src/components/feeling tracker/MoodCard.js
@@ -1,16 +1,15 @@
 import React from "react";
 import { Card} from "react-bootstrap";
-import { ReactComponent as  Happy} from "../../assets/svgComponents/Icons/Boxsmiley.svg";
-import { ReactComponent as  Unhappy} from "../../assets/svgComponents/Icons/Boxunhappy.svg";
+import { ReactComponent as Happy} from "../../assets/svgComponents/Icons/Boxsmiley.svg";
+import { ReactComponent as Unhappy} from "../../assets/svgComponents/Icons/Boxunhappy.svg";
 export default function MoodCard(props) {
 
 
-  const foundFeeling = props.feelingList.find(feeling => feeling.id === props.userFeeling.id)
+  const foundFeeling = props.feelingList.find(feeling => feeling.id === props.userFeeling.feeling_id)
   return foundFeeling.need_condition === "satisfied" ? (
    <div className="feeling_summ">
       <div className="feeling_card">
       <Happy className="happy"/>
- 
       <Card.Title>{foundFeeling.name}</Card.Title> 
       </div>
 
@@ -21,7 +20,6 @@ export default function MoodCard(props) {
       <Unhappy className="frown"/>
       <Card.Title>{foundFeeling.name}</Card.Title>
       </div>
-
    </div>
   );
 }
